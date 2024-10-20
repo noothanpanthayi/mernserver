8b2b38f9f5307a52a58327a71445280928c097dd
noothanpanthayi <noothankrishnan@outlook.com>
2024-10-20 09:46:34 -0500

    serverupdate

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const cors = require('cors');
 
 const app=express();
 app.use(cors());
-app.use('/api', [express.json(),autoRoutes])
+app.use('/api', [express.json(),autoRoutes]);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://autodealerclient.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 
 // DB Connection
 mongoose.connect(process.env.MONG_URI)
