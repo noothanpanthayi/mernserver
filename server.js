76f1672f8355a0607cd8edfd6043eae746d20044
noothanpanthayi <noothankrishnan@outlook.com>
2024-10-20 09:39:39 -0500

    updated

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const autoRoutes=require('./routes');
 const cors = require('cors');
 
 const app=express();
-// app.use(cors());
-app.use('/api', [cors(), express.json(),autoRoutes])
+app.use(cors());
+app.use('/api', [express.json(),autoRoutes])
 
 // DB Connection
 mongoose.connect(process.env.MONG_URI)
