0a4a6e8ddfc15d2828894080d6e80966b249da99
noothanpanthayi <noothankrishnan@outlook.com>
2024-10-20 09:50:36 -0500

    update

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const cors = require('cors');
 
 const app=express();
 app.use(cors());
-app.use('/api', [express.json(),autoRoutes]);
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://autodealerclient.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
+app.use('/api', [express.json(),autoRoutes]);
+
+
 
 
 // DB Connection
