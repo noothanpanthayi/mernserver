93a7f3491cc302613bd5d2c9d5cf01188bb41059
noothanpanthayi <noothankrishnan@outlook.com>
2024-10-20 00:30:44 -0500

    update

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const cors = require('cors');
 
 const app=express();
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 
-// const corsOptions={
-//     origin:"https://mernserver-wheat.vercel.app",
-//     optionsSuccessStatus:200
-// }
-// app.use(cors(corsOptions))
+const corsOptions={
+    origin:"https://mernserver-wheat.vercel.app",
+    optionsSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 // DB Connection
 mongoose.connect(process.env.MONG_URI)
